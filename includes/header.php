d2855cac6325020567cabe45a0767e8765474995
Koterba Hamza <koterbah@gmail.com>
2022-07-20 12:38:59 +0200

    création dossier + index

---
+++ includes/header.php
@@ -0,0 +1,10 @@
+<?php
+    $header = "
+    <header>
+        <ul>
+            <li><a href='index.php'>Accueil</a></li>
+            <li><a href='#'>Connexion</a></li>
+            <li><a href='#'>Inscription</a></li>
+        </ul>
+    </header>";
+?>
